6cf22c7b002d19986871cd97b9b46caca9f75d71
kamenkostov <kamen.kostov94@gmail.com>
2025-09-10 22:18:59 +0300

    added datastore to hilt and viewmodel

--- app/src/main/java/com/example/composekeepnotescopy/di/DataStoreModule.kt
+++ app/src/main/java/com/example/composekeepnotescopy/di/DataStoreModule.kt
@@ -5,8 +5,11 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 val Context.userDataStore: DataStore<Preferences> by preferencesDataStore(
@@ -16,5 +19,15 @@ val Context.userDataStore: DataStore<Preferences> by preferencesDataStore(
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
+
+
+    object {
+        @Provides
+        @Singleton
+        fun provideUserDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+            return context.userDataStore
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/composekeepnotescopy/presentation/settings/SettingsScreenViewModel.kt
+++ app/src/main/java/com/example/composekeepnotescopy/presentation/settings/SettingsScreenViewModel.kt
@@ -1,19 +1,31 @@
 package com.example.composekeepnotescopy.presentation.settings
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.composekeepnotescopy.util.DataStoreManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsScreenViewModel @Inject constructor(val dataStoreManager: DataStoreManager): ViewModel() {
+class SettingsScreenViewModel @Inject constructor(val dataStore: DataStore<Preferences>): ViewModel() {
 
     private val _state = MutableStateFlow(SettingsScreenUiState())
     val state = _state.asStateFlow()
 
     fun saveToDataStore() {
-        dataStoreManager
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStore.edit { preferences ->
+                preferences[booleanPreferencesKey("ALA_BALA")] = true
+
+            }
+        }
     }
 }
